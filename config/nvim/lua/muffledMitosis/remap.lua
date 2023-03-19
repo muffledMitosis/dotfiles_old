d1ec09071933c5f67f945465ab2f45c5aeef8dd6
Meth Munindradasa <methsndr@gmail.com>
2023-03-19 01:55:49 -0400

    Add remap to open NvimTree

--- config/nvim/lua/muffledMitosis/remap.lua
+++ config/nvim/lua/muffledMitosis/remap.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-
 -- Move blocks selected in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -14,3 +13,5 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
