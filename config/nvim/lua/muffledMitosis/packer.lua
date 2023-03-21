c3c243bde48797bd7ccab916f87b795cbd4e4bb9
Meth Munindradasa <methsndr@gmail.com>
2023-03-20 23:48:08 -0400

    Add tab completion for HTML and JSX

--- config/nvim/lua/muffledMitosis/packer.lua
+++ config/nvim/lua/muffledMitosis/packer.lua
@@ -40,4 +40,5 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-tree/nvim-tree.lua'
   use 'numToStr/Comment.nvim'
+  use 'windwp/nvim-ts-autotag'
 end)
