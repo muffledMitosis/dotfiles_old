a0e5f17232f2e410bc5b1d753646a8a17a80ca6b
Meth Munindradasa <methsndr@gmail.com>
2023-03-27 09:44:59 +0530

    Add yay and aur specific programs

--- exec/progs.sh
+++ exec/progs.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# statefile to keep track of yay install
+did_install_yay="/home/meth/dotfiles/exec/did_install_yay"
+
 # Function to figure out which linux distro
 whichOS () {
   return $(cat /etc/os-release | grep -q $1);
 }
 
-# List of programs to install using apt
+install_yay() {
+  sudo pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay && makepkg -si && cd ..
+  touch $did_install_yay
+}
+
+# List of programs to install on every distro
 programs=(
   "cool-retro-term"
   "kitty"
@@ -19,11 +29,26 @@ programs=(
   "i3"
 )
 
+programs_arch=(
+  "spotify"
+  "yad"
+  "bitwarden-cli"
+)
+
 # Install list of programs
 if $(whichOS "debian"); then
   sudo apt install ${programs[@]}
 elif $(whichOS "arch"); then
-  sudo pacman -S ${programs[@]}
+  sudo pacman -S --needed ${programs[@]}
+
+  if [ -f "$did_install_yay" ]; then
+    echo "YAY ALREADY INSTALLED"
+  else
+    install_yay
+  fi
+
+  yay -S --needed ${programs_arch[@]}
+
 else
   echo "OS NOT SUPPORTD BOI"
 fi
